38d95cc75342bccc3c8f88f674937dc379843adb
TheTinyCoder <charawe.yw@gmail.com>
2023-08-23 20:56:08 +0300

    update variable functions

--- variable_functions.c
+++ variable_functions.c
@@ -65,3 +65,107 @@ void free_var_list(var_list **head)
 		(*head) = NULL;
 	}
 }
+
+
+/**
+ * replace - function entry-point
+ *
+ * Description: replaces string into variables
+ * @head: double pointer to head of the linked list
+ * @s: string
+ * @new: new string (replaced)
+ * @len: new length
+ * Return: replaced string
+ */
+
+char *replace(var_list **head, char *s, char *new, int len)
+{
+	var_listPtr idx;
+	int i, j, k;
+
+	idx = *head;
+	for (j = i = 0; i < len; i++)
+	{
+		if (s[j] == '$')
+		{
+			if (!(idx->var_len) && !(idx->value_len))
+			{
+				new[i] = s[j];
+				j++;
+			}
+			else if (idx->var_len && !(idx->value_len))
+			{
+				for (k = 0; k < idx->var_len; k++)
+					j++;
+				i--;
+			}
+			else
+			{
+				for (k = 0; k < idx->value_len; k++)
+				{
+					new[i] = idx->value[k];
+					i++;
+				}
+				j += (idx->var_len);
+				i--;
+			}
+			idx = idx->next;
+		}
+		else
+		{
+			new[i] = s[j];
+			j++;
+		}
+	}
+
+	return (new);
+}
+
+
+/**
+ * replaced_var - function entry-point
+ *
+ * Description: calls functions to replace string into var
+ * @s: string
+ * @data: shell data structure
+ * Return: replaced string
+ */
+char *replaced_var(char *s, data_t *data)
+{
+	var_listPtr head, idx;
+	char *status, *new;
+	int olen, nlen;
+
+	status = _itoa(data->status);
+	head = NULL;
+
+	olen = check_for_vars(&head, s, status, data);
+
+	if (head == NULL)
+	{
+		free(status);
+		return (s);
+	}
+
+	idx = head;
+	nlen = 0;
+
+	while (idx != NULL)
+	{
+		nlen += (idx->value_len - idx->var_len);
+		idx = idx->next;
+	}
+
+	nlen += olen;
+
+	new = malloc(sizeof(char) * (nlen + 1));
+	new[nlen] = '\0';
+
+	new = replace(&head, s, new, nlen);
+
+	free(s);
+	free(status);
+	free_var_list(&head);
+
+	return (new);
+}
