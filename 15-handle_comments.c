4112b330dcf4b9f9ef3e63377d2c42d4482bb438
TheTinyCoder <charawe.yw@gmail.com>
2023-08-23 21:00:00 +0300

    handle task 15

---
+++ 15-handle_comments.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * handle_comment - function entry-point
+ *
+ * Description: removes comments from the input
+ * @s: string
+ * Return: string with no comments
+ */
+char *handle_comment(char *s)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(s);
+				return (NULL);
+			}
+
+			if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == ';')
+				j = i;
+		}
+	}
+
+	if (j != 0)
+	{
+		s = _realloc(s, i, j + 1);
+		s[j] = '\0';
+	}
+
+	return (s);
+}
